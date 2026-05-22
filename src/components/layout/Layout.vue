a286be5997395831361939b2c0df9ef6fdcea784
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-22 21:17:04 +0900

    Create Layout.vue

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from './Header.vue'
+import Sidebar from './Sidebar.vue'
+</script>
+
+<template>
+  <Header />
+  <Sidebar />
+
+  <main class="pt-16 pl-60">
+    <slot />
+  </main>
+</template>

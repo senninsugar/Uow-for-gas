6f9f2a6eb11363d77fa70d345a6f9d770ef9b899
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-22 21:18:11 +0900

    Create VideoGrid.vue

---
+++ src/components/layout/video/VideoGrid.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import VideoCard from './VideoCard.vue'
+
+defineProps<{
+  videos: any[]
+}>()
+</script>
+
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 p-6">
+    <VideoCard
+      v-for="video in videos"
+      :key="video.videoId"
+      :video="video"
+    />
+  </div>
+</template>

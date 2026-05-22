c441880098557900aed7e7f36bed73d845f933f6
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-22 21:18:48 +0900

    Create VideoPlayer.vue

---
+++ src/components/layout/video/VideoPlayer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  id: string
+}>()
+</script>
+
+<template>
+  <iframe
+    class="w-full aspect-video rounded-2xl"
+    :src="`https://www.youtube.com/embed/${id}`"
+    allowfullscreen
+  />
+</template>

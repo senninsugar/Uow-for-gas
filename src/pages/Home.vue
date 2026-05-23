d99bdc4b90b49558f802f759ad5866dc76d15386
senninsugar <hiroki20134@gmail.com>
2026-05-24 07:21:08 +0900

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,56 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+
 import Layout from '../components/layout/Layout.vue'
+
 import VideoGrid from '../components/video/VideoGrid.vue'
-import { trendingVideos } from '../api/invidious'
+
+import Loading from '../components/layout/Loading.vue'
 
 const videos = ref<any[]>([])
 
+const loading = ref(true)
+
 onMounted(async () => {
-  videos.value = await trendingVideos()
+  try {
+    const res = await fetch(
+      'https://raw.githubusercontent.com/ajgpw/youtubedata/refs/heads/main/trend-base64.json'
+    )
+
+    const data = await res.json()
+
+    videos.value = data.trending.map((video: any) => ({
+      videoId: video.id,
+
+      title: video.title,
+
+      author: video.channel,
+
+      authorId: video.channelId,
+
+      published: video.publishedAt,
+
+      videoThumbnails: [
+        {
+          url: video.thumbnails.standard.url
+        }
+      ]
+    }))
+  } finally {
+    loading.value = false
+  }
 })
 </script>
 
 <template>
   <Layout>
-    <VideoGrid :videos="videos" />
+
+    <Loading v-if="loading" />
+
+    <VideoGrid
+      v-else
+      :videos="videos"
+    />
+
   </Layout>
 </template>

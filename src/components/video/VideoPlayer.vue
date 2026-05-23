15d9b8bd80dcf17ff179556023de3e5e114d4a79
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-23 21:08:32 +0900

    Update VideoPlayer.vue

--- src/components/video/VideoPlayer.vue
+++ src/components/video/VideoPlayer.vue
@@ -1,19 +1,58 @@
 <!-- components/video/VideoPlayer.vue -->
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue'
+
+const props = defineProps<{
   id: string
   stream?: string
+  formatStreams?: any[]
 }>()
+
+const selectedVideo = computed(() => {
+  if (!props.formatStreams?.length) {
+    return null
+  }
+
+  return (
+    props.formatStreams.find(
+      (stream) =>
+        stream.qualityLabel === '720p'
+    ) ||
+    props.formatStreams.find(
+      (stream) =>
+        stream.qualityLabel === '480p'
+    ) ||
+    props.formatStreams[0]
+  )
+})
 </script>
 
 <template>
-  <iframe
-    class="w-full aspect-video rounded-2xl border border-zinc-200"
-    :src="
-      stream === 'youtube'
-        ? `https://www.youtube.com/embed/${id}`
-        : `https://www.youtube-nocookie.com/embed/${id}`
-    "
-    allowfullscreen
-  />
+  <div
+    class="w-full aspect-video rounded-2xl border border-zinc-200 overflow-hidden bg-black"
+  >
+    <video
+      v-if="selectedVideo"
+      class="w-full h-full"
+      controls
+      autoplay
+      playsinline
+    >
+      <source
+        :src="selectedVideo.url"
+        :type="selectedVideo.type"
+      >
+    </video>
+
+    <iframe
+      v-else
+      class="w-full h-full"
+      :src="
+        stream === 'youtube'
+          ? `https://www.youtube.com/embed/${id}`
+          : `https://www.youtube-nocookie.com/embed/${id}`
+      "
+      allowfullscreen
+    />
+  </div>
 </template>
